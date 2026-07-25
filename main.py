1abf20a49ba42d7d5da7d6d27afbe187908c5eda
Kshitij Arya <kshitijcrusader36@gmail.com>
2026-07-26 01:26:16 +0530

    added infinite loop for the game.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Screen
 from snake import Snake
 
@@ -5,8 +6,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-#screen.tracer(0)
+screen.tracer(0)
 
 snake = Snake()
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
